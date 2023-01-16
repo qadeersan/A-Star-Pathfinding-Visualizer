a9b7c1f55ddf3a10d9bd65eb05099663fd182210
qadeerassan <45184523+qadeerassan@users.noreply.github.com>
2023-01-16 22:23:01 +0000

    linked Django and React and cleared f/e canvas

--- djangobackend/astar/urls.py
+++ djangobackend/astar/urls.py
@@ -9,6 +9,6 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index.html'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
