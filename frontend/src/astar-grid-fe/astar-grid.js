65a074bc0164be70c37bf8f3411f06b6a6235834
qadeerassan <45184523+qadeerassan@users.noreply.github.com>
2023-01-17 23:03:30 +0000

    centered and coloured grid

--- frontend/src/astar-grid-fe/astar-grid.js
+++ frontend/src/astar-grid-fe/astar-grid.js
@@ -2,19 +2,19 @@ import React, { useState, useEffect } from 'react';
 import './astar-grid.css'
 
 const GridInterface = () => {
-    const [grid, setGrid] = useState(Array(100).fill().map(() => Array(100).fill('|')));
+    const [grid, setGrid] = useState(Array(100).fill().map(() => Array(100).fill('violet')));
     const [isMouseDown, setIsMouseDown] = useState(false);
-    const [elementSize, setElementSize] = useState(10);
+    const [elementWidth, setElementWidth] = useState(3);
+    const [currentState, setCurrentState] = useState("barrier"); //new state variable to keep track of the current state
 
     useEffect(() => {
         const handleResize = () => {
-            const width = window.innerWidth;
-            const height = window.innerHeight;
-            setElementSize(Math.min(width / 100, height / 100));
+            setElementWidth(Math.floor(window.innerHeight / 120));
         };
         handleResize();
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
+        window.addEventListener("resize", handleResize);
+        setTimeout(handleResize, 50);
+        return () => window.removeEventListener("resize", handleResize);
     }, []);
 
     const handleMouseDown = (i, j) => {
@@ -28,38 +28,69 @@ const GridInterface = () => {
 
     const handleClick = (i, j) => {
         if (!isMouseDown) return;
-        const newGrid = [...grid];
-        newGrid[i][j] = newGrid[i][j] === '|' ? '1' : '|';
-        setGrid(newGrid);
+        let newColor;
+        switch (currentState) {
+            case "barrier":
+                newColor = "black";
+                break;
+            case "start":
+                newColor = "green";
+                break;
+            case "end":
+                newColor = "red";
+                break;
+            default:
+                newColor = "violet";
+        }
+
+        let newGrid = [...grid];
+
+        if (currentState === "clear") {
+            newGrid = [grid]
+            setGrid(newGrid)
+        } else {
+            newGrid[i][j] = newColor;
+            setGrid(newGrid);
+        }
+    }
+
+    const handleStateChange = (newState) => {
+        setCurrentState(newState);
     }
 
     return (
-        <div>
+        <span className='grid-component'>
+        <div className="grid-container">
+            <button onClick={() => handleStateChange("barrier")}>Barrier</button>
+            <button onClick={() => handleStateChange("start")}>Start Point</button>
+            <button onClick={() => handleStateChange("end")}>End Point</button>
+            <button onClick={() => handleStateChange("clear")}>Clear</button>
             <table>
                 <tbody>
                 {grid.map((row, i) => (
                     <tr key={i}>
-                                                {row.map((col, j) => (
+                        {row.map((col, j) => (
                             <td key={j} 
-                                style={{ 
-                                    width: elementSize + 'px', 
-                                    height: elementSize + 'px', 
-                                    backgroundColor: grid[i][j] 
+                            className="grid-element"
+                            style={{ 
+                                width: elementWidth,
+                                height: elementWidth,
+                                backgroundColor: grid[i][j]
                                 }} 
                                 onMouseDown={() => handleMouseDown(i, j)}
                                 onMouseUp={handleMouseUp}
                                 onMouseEnter={() => handleClick(i, j)}
-                            >
-                                {grid[i][j]}
-                            </td>
-                        ))}
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </div>
-    );
-
-}
-
-export default GridInterface;
+                                >
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+            </span>
+        );
+    }
+    
+    export default GridInterface;
+    
