8c4add9942acea87b8824e22060d8ecb80ac8fed
qadeerassan <45184523+qadeerassan@users.noreply.github.com>
2023-01-22 07:07:01 +0000

    added heuristic and node neighbours functions

---
+++ djangobackend/astar/pathfinding.py
@@ -0,0 +1,12 @@
+def heuristic(i1, j1, i2, j2):
+    return abs(i2 - i1) + abs(j2 - j1)
+
+def get_neighbours(i,j, barriers, neighbours):
+    if i < 100 and [i+1,j] not in barriers:  #DOWN
+        neighbours.append([i+1,j])
+    if i >= 0 and j < 100 and [i-1,j] not in barriers:  #UP
+        neighbours.append([i+1,j])
+    if j < 100 and [i,j+1] not in barriers:  #RIGHT
+        neighbours.append([i+1,j])
+    if j >= 0 and [i,j-1] not in barriers:  #LEFT
+        neighbours.append([i+1,j])
