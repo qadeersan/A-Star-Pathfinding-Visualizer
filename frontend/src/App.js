0e9eb7920e9ca532eed674ff4173780beed89cd7
qadeerassan <45184523+qadeerassan@users.noreply.github.com>
2023-01-22 05:12:06 +0000

    nomenclature update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import GridInterface from './fe-Grid/gridInterface';
+import GridInterface from './grid-ui/gridInterface';
 
 function App() {
   return (
