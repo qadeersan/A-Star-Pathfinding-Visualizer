025ccb48e8c770841baf8f30c3e8545dba0f37d3
qadeerassan <45184523+qadeerassan@users.noreply.github.com>
2023-01-23 06:55:10 +0000

    neighbours and input submit code

--- djangobackend/astar/pathfinding.py
+++ djangobackend/astar/pathfinding.py
@@ -5,8 +5,9 @@ def get_neighbours(i,j, barriers, neighbours):
     if i < 100 and [i+1,j] not in barriers:  #DOWN
         neighbours.append([i+1,j])
     if i >= 0 and [i-1,j] not in barriers:  #UP
-        neighbours.append([i+1,j])
+        neighbours.append([i-1,j])
     if j < 100 and [i,j+1] not in barriers:  #RIGHT
-        neighbours.append([i+1,j])
+        neighbours.append([i,j+1])
     if j >= 0 and [i,j-1] not in barriers:  #LEFT
-        neighbours.append([i+1,j])
+        neighbours.append([i,j-1])
+
--- frontend/src/grid-ui/gridButtons.js
+++ frontend/src/grid-ui/gridButtons.js
@@ -1,5 +1,7 @@
+import axios from 'axios';
+
 const GridButtons = (props) => {
-    const { setEnd, setStart, setBarriers, setGrid, setClick } = props;
+    const { start, end, barriers, setEnd, setStart, setBarriers, setGrid, setClick } = props;
 
     const handleReset = () => {
         setStart(null);
@@ -8,10 +10,28 @@ const GridButtons = (props) => {
         setGrid(Array(100).fill().map(() => Array(100).fill('violet')));
         setClick(1);
       }
+
+      const handleSubmit = async () => {
+        // Prepare the data to send to the backend
+        const data = {
+            start: start,
+            end: end,
+            barriers: barriers
+        }
+    
+        // Send a POST request to the backend
+        try {
+            const response = await axios.post('/api/pathfinding/', data);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
       
     return (
         <div>
             <button onClick={handleReset}>Reset</button>
+            <button onClick={handleSubmit}>Submit</button>
         </div>
     );
 }
